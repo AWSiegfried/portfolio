b4d159e442d92902ec28fbed1bd65c64ef1c5477
AWSiegfried <awsiegfried@gmail.com>
2021-02-25 17:11:02 -0800

    about page height

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./About.css"
 
 function About() {
     return (
-        <div>
+        <div className="about-container">
             About
         </div>
     )
