c7f94eea185c2103188c8a15e179a816cb240368
AWSiegfried <awsiegfried@gmail.com>
2021-02-25 16:30:32 -0800

    initial files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+//React
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Styles
+import './App.css';
+
+//Routes
+
+function App() {
+  return (
+    <div className="main-div-container">
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/AllPages.js
@@ -0,0 +1,12 @@
+import About from "./About/About"
+import Contact from "./Contact/Contact"
+import Home from "./Home/Home"
+import Projects from "./Projects/Projects"
+
+export default About;
+export default Contact;
+export default Home;
+export default Projects;
+
+
+
