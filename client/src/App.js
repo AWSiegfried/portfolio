31effbdd103467b0619a983a7496f34e31ec58c4
AWSiegfried <awsiegfried@gmail.com>
2021-02-25 16:42:20 -0800

    app basic routes and imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 //React
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Styles
-import './App.css';
+import "./App.css";
 
 //Routes
-import { About, Contact, Home, Projects } from "./pages/AllPages"
-
+import { About, Contact, Home, Projects } from "./pages/allPages";
+import { Navbar, Footer } from "./components/allComponents";
 function App() {
   return (
     <div className="main-div-container">
       <Router>
-        
+        <Navbar />
+        <Switch>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/ontact" component={Contact}></Route>
+          <Route exact path="/projects" component={Projects}></Route>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+        <Footer />
       </Router>
     </div>
   );
