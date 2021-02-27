6854ae7ed80d4b8303b98443b76cb2e68bcb2a34
AWSiegfried <awsiegfried@gmail.com>
2021-02-26 17:48:25 -0800

    background export file

---
+++ client/src/assets/backgrounds/allbackgrounds.js
@@ -0,0 +1,11 @@
+import About from "./about.jpg";
+import Contact from "./contact.jpg";
+import Home from "./home.jpg";
+import Projects from "./projects.jpg";
+
+export {
+    About, 
+    Contact,
+    Home,
+    Projects
+}
