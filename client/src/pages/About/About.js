295cd154b562a7fd821fd760266dee504d0d822b
AWSiegfried <awsiegfried@gmail.com>
2021-03-01 14:12:48 -0800

    home styling and text

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./About.css"
 
+//Background
+import {About as about} from "../../assets/backgrounds/allbackgrounds"
+
 function About() {
+
+    const styles= {
+        background: {
+            backgroundImage: `url("${about}")`,
+            padding: "0",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundAttachment: "fixed",
+            backgroundRepeat: "no-repeat",
+            // marginBottom: "0"
+
+        }
+    }
     return (
-        <div className="about-container">
+        <div className="about-container" style={styles.background}>
             About
         </div>
     )
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+
+//Styles
 import "./Home.css";
 
+//Background
+import {Home as home} from "../../assets/backgrounds/allbackgrounds"
+
 function Home() {
-    return (
-        <div className="home-container">
-            Home
-        </div>
-    )
+
+    const styles= {
+        background: {
+            backgroundImage: `url("${home}")`,
+            padding: "0",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundAttachment: "fixed",
+            backgroundRepeat: "no-repeat",
+            // marginBottom: "0"
+
+        },
+        p: {
+            marginBottom: ".5rem"
+        }
+    }
+  return (
+    <div className="home-container" style={styles.background}>
+      <div className="home-text">
+        <h1>Hello, I'm Alex</h1>
+        <p style={styles.p}>web-developer</p>
+        <p style={styles.p}>musician</p>
+        <p style={styles.p}>maker of pretty things</p>
+        <Nav.Link
+          href="/about"
+          className="learn-more pl-0"
+          style={{
+            color: "#9900C9",
+            fontWeight: "400" 
+          }}
+          // style={styles.NavbarLink}
+        >
+            Learn More
+        </Nav.Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
