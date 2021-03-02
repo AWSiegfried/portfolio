b7dee0d6468c0ba82f4e4efe45e7aa9f0dc64519
AWSiegfried <awsiegfried@gmail.com>
2021-03-02 09:04:24 -0800

    font styling media queries

--- client/src/components/Title/Title.js
+++ client/src/components/Title/Title.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./Title.css"
 
-function Title(pageTitle) {
+function Title() {
   const location = useLocation();
 
   return (
-    <div>
-      { (location.pathname.includes("about")) ? "About"
+    <div className="title-text">
+      { (location.pathname.includes("about")) ? "About Me"
         : location.pathname.includes("projects") ? "Projects"
         : "Contact"}
     </div>
--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,36 +1,54 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
 //Background
-import {About as about} from "../../assets/backgrounds/allbackgrounds"
+import { About as about } from "../../assets/backgrounds/allbackgrounds";
 
 //Picture
-import alex from "../../assets/images/alex.png"
+import alex from "../../assets/images/alex.png";
 
 //Components
-import { Title } from "../../components/allComponents"
+import { Title } from "../../components/allComponents";
 
 function About() {
-
-    const styles= {
-        background: {
-            backgroundImage: `url("${about}")`,
-            padding: "0",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundAttachment: "fixed",
-            backgroundRepeat: "no-repeat",
-            // marginBottom: "0"
-        }
-    }
-    return (
-        <div className="about-container" style={styles.background}>
-            <img src={alex} alt="Bio"/>
-            <Title/>
-            <div style={{color: "white"}}>Text</div>
-            
-        </div>
-    )
+  const styles = {
+    background: {
+      backgroundImage: `url("${about}")`,
+      padding: "0",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundAttachment: "fixed",
+      backgroundRepeat: "no-repeat",
+      // marginBottom: "0"
+    },
+  };
+  return (
+    <div className="about-container" style={styles.background}>
+      <img src={alex} alt="Bio" className="bio-picture"/>
+      <div className="text-container">
+        <Title />
+        <p>
+          Nulla euismod elementum tortor sed laoreet. Maecenas vel sucipit
+          lacus. Pellentesque egestas, tellus sed accumsan vulputate, magna nisi
+          vulputate magna, non lacinia nisl sapien nec mauris. 
+          <br></br>
+          <br></br>
+          Nunc imperdiet
+          nibh ac odio scelerisque laoreet. Sed et ligula sit amet velit
+          consectetur fermentum a feugiat massa. Aliquam ex quam, fringilla ac
+          ligula vitae, semper dapibus risus. Nullam at ipsum sed erat malesuada
+          lacinia ac ut quam. Ut vel laoreet est.
+        </p>
+        <p><span>Languages: </span>HTML, Javascript, CSS</p>
+        <p><span>Technical skills: </span>React, Redux, MERN, MySQL, Material UI, etc.</p>
+        <p><span>Competencies: </span>
+        Business development, brand building, 
+	advertising campaing/management
+        </p>
+        
+      </div>
+    </div>
+  );
 }
 
 export default About;
