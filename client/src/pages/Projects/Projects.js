3a0ac77c54a690ce0c998e7f5d793e862104a77d
AWSiegfried <awsiegfried@gmail.com>
2021-03-02 09:24:36 -0800

    projects background added

--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
+
+//Styles
 import "./Projects.css"
 
 //Components
-import {Title} from "../../components/allComponents"
+import { Title } from "../../components/allComponents";
+import {Projects as background } from "../../assets/backgrounds/allbackgrounds"
 
 function Projects() {
-    return (
-        <div className="projects-container">
-            <Title/>
-        </div>
-    )
+  const styles = {
+    background: {
+      backgroundImage: `url("${background}")`,
+    //   padding: "0",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundAttachment: "fixed",
+      backgroundRepeat: "no-repeat",
+      // marginBottom: "0"
+    },
+    p: {
+      marginBottom: ".5rem",
+    },
+  };
+
+  return (
+    <div className="projects-container" style={styles.background}>
+      <Title />
+    </div>
+  );
 }
 
 export default Projects;
